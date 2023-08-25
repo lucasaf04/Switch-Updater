99a237428946e1a457b6009cff9feaee675aabc5
Lucas Alonso <39613949+lucasaf04@users.noreply.github.com>
2023-08-25 10:57:00 +0200

    Minor `main.py` changes.

--- src/main.py
+++ src/main.py
@@ -89,7 +89,7 @@ def download_all(
                     )
 
 
-def create_payload() -> None:
+def create_root_payload() -> None:
     for file in ROOT_SAVE_PATH.iterdir():
         if re.search(r"hekate_ctcaer_(?:\d+\.\d+\.\d+)\.bin", file.name) is not None:
             new_path = file.with_name("payload.bin")
@@ -117,22 +117,21 @@ def move_nro_apps_into_folders() -> None:
             item.rename(folder / item.name)
 
 
-def valid_log_level(level: str) -> str:
-    numeric_level = getattr(logging, level.upper(), None)
-    if not isinstance(numeric_level, int):
-        raise ArgumentTypeError(f"Invalid log level: `{level}`")
-    return level
+def create_cli_parser() -> ArgumentParser:
+    def valid_log_level(level: str) -> str:
+        numeric_level = getattr(logging, level.upper(), None)
+        if not isinstance(numeric_level, int):
+            raise ArgumentTypeError(f"Invalid log level: `{level}`")
+        return level
 
-
-def main() -> None:
-    valid_log_levels = logging.getLevelNamesMapping().keys()
+    valid_log_levels = ", ".join(logging.getLevelNamesMapping().keys())
 
     cli_parser = ArgumentParser()
     cli_parser.add_argument(
         "--log",
         type=valid_log_level,
         default="ERROR",
-        help=f"Set the log level: {', '.join(valid_log_levels)} (default ERROR)",
+        help=f"Set the log level: {valid_log_levels} (default ERROR)",
     )
     cli_parser.add_argument("--mariko", action="store_true", help="Enable mariko mode")
     cli_parser.add_argument(
@@ -142,6 +141,11 @@ def main() -> None:
         "--remove-cache", action="store_true", help="Delete previously downloaded files"
     )
     cli_parser.add_argument("--pack", help="Name of the zip file to create")
+    return cli_parser
+
+
+def main() -> None:
+    cli_parser = create_cli_parser()
     cli_args = cli_parser.parse_args()
 
     logging.basicConfig(
@@ -174,7 +178,7 @@ def main() -> None:
     save_downloads_lock(downloads_lock_list)
 
     if cli_args.mariko:
-        create_payload()
+        create_root_payload()
         remove_from_root(["switch/reboot_to_payload.nro"])
     else:
         # todo
